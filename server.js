1dfce3bb5b8846513ce8e56d585466be29d728ba
Shema Rezanejad <shemarez@uw.edu>
2016-11-20 20:50:34 -0800

    user can now register

--- server.js
+++ server.js
@@ -17,14 +17,84 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8888);
 
+//serve static files in the public folder
+app.use('/public', require('express').static(path.join(__dirname + '/public')));
+
+//middleware for passing data bewteen routes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+server.listen(app.get('port'), function() {
+	console.log('listening on port:', app.get('port'));
+});
+
 //login page
 app.get('/', function (req, res) {
   	res.render('login.ejs');
+});
+app.post('/login', function(req, res) {
+	 var email = req.body.email;
+    var password = req.body.password;
+
+    // pool.getConnection(function(err, connection) {
+    //     connection.query("SELECT * FROM faculty WHERE faculty.email = " + "'" + email + "'" + "AND faculty.password = " + "'" + password + "'", function(err, rows) {
+    //         if (err) {
+    //             console.log(err);
+    //         } else {
+    //             console.log(rows);
+    //         }
+
+    //         //user found
+    //         if (rows.length) {
+    //             console.log('user found');
+    //             req.session.userId = rows[0].id;
+    //             console.log(req.session.userId);
+               
+    //             res.redirect('/homepage');
+    //         } else {
+    //             console.log('user not found');
+    //             res.send('error user not found');
+    //         }
+
+    //     });
+    // });
+
 });
 
 //register page
 app.get('/register', function(req, res) {
 	res.render('register.ejs');
+});
+
+
+//POST to register
+app.post('/register', function(req, res) {
+	// console.dir(req.body);
+	console.log("email: " + req.body.email);
+	console.log("password: " + req.body.password);
+
+	// getting inputted username and pass
+	var email = req.body.email;
+	var password = req.body.password;
+
+	// connect and insert into database
+	 pool.getConnection(function(err, connection) {
+        connection.query("INSERT INTO faculty (email, password) VALUES ('" + email + "'" + "," + "'" + password + "'" + ")", function(err, rows) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(rows);
+                console.log("User was successfully registered")
+                res.redirect('/'); // once registered redirect to login page
+            }
+
+            // verify user doesnt already have an account
+            // verify it is a valid email address
+            // verify @ and .com/.edu/.net/.org included aka it is a complete email
+            connection.release();
+        });
+
+    });
 });
 
 //survey page
@@ -66,13 +136,3 @@ app.get('/report', function(req, res) {
 	res.render('report.ejs');
 });
 
-//serve static files in the public folder
-app.use('/public', require('express').static(path.join(__dirname + '/public')));
-
-//middleware for passing data bewteen routes
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-server.listen(app.get('port'), function() {
-	console.log('listening on port:', app.get('port'));
-});
